221150f18cdb00c154b08807ca206a11b47549af
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-24 22:48:42 +0800

    remove unused navbar

--- lib/screen/main/main_screen.dart
+++ lib/screen/main/main_screen.dart
@@ -1,44 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_restaurant_app/provider/main/index_nav_provider.dart';
-// ignore: unused_import
-import 'package:flutter_restaurant_app/screen/bookmark/bookmark_screen.dart';
 import 'package:flutter_restaurant_app/screen/home/home_screen.dart';
-import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Consumer<IndexNavProvider>(
-        builder: (context, value, child) {
-          return switch (value.indexBottomNavBar) {
-            0 => const HomeScreen(),
-            // _ => const BookmarkScreen(),
-            // TODO: Handle this case.
-            int() => throw UnimplementedError(),
-          };
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: context.watch<IndexNavProvider>().indexBottomNavBar,
-        onTap: (index) {
-          context.read<IndexNavProvider>().setIndextBottomNavBar = index;
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-            tooltip: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bookmarks),
-            label: "Bookmarks",
-            tooltip: "Bookmarks",
-          ),
-        ],
-      ),
-    );
+    return const Scaffold(body: HomeScreen());
   }
 }

d3ddd6b29643599d87a96f8ffbe4c0fb09d2fe65
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-24 23:54:41 +0800

    remove comments

--- lib/provider/detail/restaurant_detail_provider.dart
+++ lib/provider/detail/restaurant_detail_provider.dart
@@ -1,20 +1,16 @@
-// todo-03-detail-01: create a new provider to handle a detail api
 import 'package:flutter/widgets.dart';
 import 'package:flutter_restaurant_app/data/api/api_services.dart';
 import 'package:flutter_restaurant_app/static/restaurant_detail_result_state.dart';
 
 class RestaurantDetailProvider extends ChangeNotifier {
-  // todo-03-detail-02: inject an ApiService to maintain more easily
   final ApiServices _apiServices;
 
   RestaurantDetailProvider(this._apiServices);
 
-  // todo-03-detail-03: add a variable state and get method
   RestaurantDetailResultState _resultState = RestaurantDetailNoneState();
 
   RestaurantDetailResultState get resultState => _resultState;
 
-  // todo-03-detail-04: create a function to load a detail page
   Future<void> fetchRestaurantDetail(String id) async {
     try {
       _resultState = RestaurantDetailLoadingState();

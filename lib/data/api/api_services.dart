8299fe55961f8fc3d01cee782dd1587b24ee719e
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-24 12:55:22 +0800

    create restaurant detail response

--- lib/data/api/api_services.dart
+++ lib/data/api/api_services.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 
 // ignore: unused_import
-import 'package:flutter_restaurant_app/data/model/restaurant.dart';
+import 'package:flutter_restaurant_app/data/model/restaurant_model.dart';
 import 'package:flutter_restaurant_app/data/model/restaurant_list_response.dart';
 // ignore: unused_import
-import 'package:flutter_restaurant_app/data/model/tourism_detail_response.dart';
+import 'package:flutter_restaurant_app/data/model/restaurant_detail_response.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServices {
@@ -20,15 +20,14 @@ class ApiServices {
       throw Exception('Failed to load restaurant list');
     }
   }
+
+  Future<RestaurantDetailResponse> getRestaurantDetail(String id) async {
+    final response = await http.get(Uri.parse("$_baseUrl/detail/$id"));
+
+    if (response.statusCode == 200) {
+      return RestaurantDetailResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load tourism detail');
+    }
+  }
 }
-
-
-  // Future<TourismDetailResponse> getTourismDetail(int id) async {
-  //   final response = await http.get(Uri.parse("$_baseUrl/detail/$id"));
-
-  //   if (response.statusCode == 200) {
-  //     return TourismDetailResponse.fromJson(jsonDecode(response.body));
-  //   } else {
-  //     throw Exception('Failed to load tourism detail');
-  //   }
-  // }

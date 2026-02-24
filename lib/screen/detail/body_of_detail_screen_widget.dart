781b45279a90a14fdcddcf8a7c76696b26e060a4
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-24 12:57:11 +0800

    update body of detail screen widget

--- lib/screen/detail/body_of_detail_screen_widget.dart
+++ lib/screen/detail/body_of_detail_screen_widget.dart
@@ -1,61 +1,66 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_restaurant_app/data/model/restaurant.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_restaurant_app/data/model/restaurant_detail_model.dart';
 
-// class BodyOfDetailScreenWidget extends StatelessWidget {
-//   const BodyOfDetailScreenWidget({super.key, required this.tourism});
+class BodyOfDetailScreenWidget extends StatelessWidget {
+  const BodyOfDetailScreenWidget({super.key, required this.restaurantDetail});
 
-//   final Tourism tourism;
+  final RestaurantDetail restaurantDetail;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           children: [
-//             Image.network(tourism.image, fit: BoxFit.cover),
-//             const SizedBox.square(dimension: 16),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 Expanded(
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text(
-//                         tourism.name,
-//                         style: Theme.of(context).textTheme.headlineLarge,
-//                       ),
-//                       Text(
-//                         tourism.address,
-//                         style: Theme.of(context).textTheme.labelLarge?.copyWith(
-//                           fontWeight: FontWeight.w400,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Row(
-//                   children: [
-//                     const Icon(Icons.favorite, color: Colors.pink),
-//                     const SizedBox.square(dimension: 4),
-//                     Text(
-//                       tourism.like.toString(),
-//                       style: Theme.of(context).textTheme.bodyLarge,
-//                     ),
-//                   ],
-//                 ),
-//               ],
-//             ),
-//             const SizedBox.square(dimension: 16),
-//             Text(
-//               tourism.description,
-//               style: Theme.of(context).textTheme.bodyLarge,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image.network(
+              "https://restaurant-api.dicoding.dev/images/small/${restaurantDetail.pictureId}",
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) =>
+                  const Icon(Icons.broken_image),
+            ),
+            const SizedBox.square(dimension: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        restaurantDetail.name,
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
+                      Text(
+                        restaurantDetail.address,
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Icon(Icons.favorite, color: Colors.pink),
+                    const SizedBox.square(dimension: 4),
+                    Text(
+                      restaurantDetail.rating.toString(),
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox.square(dimension: 16),
+            Text(
+              restaurantDetail.description,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

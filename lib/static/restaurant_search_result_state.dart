e9fe952334fdd02c52fd3c1086af7d161a3af8fd
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-24 14:50:42 +0800

    create restaurant search result state

---
+++ lib/static/restaurant_search_result_state.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_restaurant_app/data/model/restaurant_model.dart';
+
+sealed class RestaurantSearchResultState {}
+
+class RestaurantSearchNoneState extends RestaurantSearchResultState {}
+
+class RestaurantSearchLoadingState extends RestaurantSearchResultState {}
+
+class RestaurantSearchErrorState extends RestaurantSearchResultState {
+  final String error;
+  RestaurantSearchErrorState(this.error);
+}
+
+class RestaurantSearchLoadedState extends RestaurantSearchResultState {
+  final List<Restaurant> data;
+  RestaurantSearchLoadedState(this.data);
+}

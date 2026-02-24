f576aa79ed906beb7d605ca9186432d755f87eae
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-24 14:50:14 +0800

    add search button to the home screen

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_restaurant_app/provider/home/restaurant_list_provider.dart';
 import 'package:flutter_restaurant_app/screen/home/restaurant_card_widget.dart';
+import 'package:flutter_restaurant_app/screen/search/search_screen.dart';
 import 'package:flutter_restaurant_app/static/navigation_route.dart';
 import 'package:flutter_restaurant_app/static/restaurant_list_result_state.dart';
 import 'package:provider/provider.dart';
@@ -31,7 +32,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Restaurant List")),
+      appBar: AppBar(
+        title: const Text("Restaurant List"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const SearchScreen()),
+              );
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
       // todo-02-home-10: comment this code below
       // todo-02-home-11: add a Consumer to maintain the result state
       body: Consumer<RestaurantListProvider>(

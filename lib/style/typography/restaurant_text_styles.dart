8774982265646f89c6e7b98af0f70821ba76016a
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-25 00:49:35 +0800

    remove comments

--- lib/style/typography/restaurant_text_styles.dart
+++ lib/style/typography/restaurant_text_styles.dart
@@ -4,7 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 class RestaurantTextStyles {
   static TextStyle get _base => GoogleFonts.montserrat();
 
-  /// displayLarge Text Style
   static TextStyle displayLarge = _base.copyWith(
     fontSize: 57,
     fontWeight: FontWeight.w700,
@@ -12,7 +11,6 @@ class RestaurantTextStyles {
     letterSpacing: -2,
   );
 
-  /// displayMedium Text Style
   static TextStyle displayMedium = _base.copyWith(
     fontSize: 45,
     fontWeight: FontWeight.w600,
@@ -20,7 +18,6 @@ class RestaurantTextStyles {
     letterSpacing: -1,
   );
 
-  /// displaySmall Text Style
   static TextStyle displaySmall = _base.copyWith(
     fontSize: 36,
     fontWeight: FontWeight.w500,
@@ -28,7 +25,6 @@ class RestaurantTextStyles {
     letterSpacing: -1,
   );
 
-  /// headlineLarge Text Style
   static TextStyle headlineLarge = _base.copyWith(
     fontSize: 32,
     fontWeight: FontWeight.w600,
@@ -36,7 +32,6 @@ class RestaurantTextStyles {
     letterSpacing: -1,
   );
 
-  /// headlineMedium Text Style
   static TextStyle headlineMedium = _base.copyWith(
     fontSize: 28,
     fontWeight: FontWeight.w500,
@@ -44,7 +39,6 @@ class RestaurantTextStyles {
     letterSpacing: -1,
   );
 
-  /// headlineMedium Text Style
   static TextStyle headlineSmall = _base.copyWith(
     fontSize: 24,
     fontWeight: FontWeight.w400,
@@ -52,7 +46,6 @@ class RestaurantTextStyles {
     letterSpacing: -1,
   );
 
-  /// titleLarge Text Style
   static TextStyle titleLarge = _base.copyWith(
     fontSize: 22,
     fontWeight: FontWeight.w500,
@@ -60,7 +53,6 @@ class RestaurantTextStyles {
     letterSpacing: 1.2,
   );
 
-  /// titleMedium Text Style
   static TextStyle titleMedium = _base.copyWith(
     fontSize: 16,
     fontWeight: FontWeight.w400,
@@ -68,7 +60,6 @@ class RestaurantTextStyles {
     letterSpacing: 1.2,
   );
 
-  /// titleSmall Text Style
   static TextStyle titleSmall = _base.copyWith(
     fontSize: 14,
     fontWeight: FontWeight.w300,
@@ -76,28 +67,24 @@ class RestaurantTextStyles {
     letterSpacing: 1.2,
   );
 
-  /// bodyLargeBold Text Style
   static TextStyle bodyLargeBold = _base.copyWith(
     fontSize: 16,
     fontWeight: FontWeight.w400,
     height: 1.56,
   );
 
-  /// bodyLargeMedium Text Style
   static TextStyle bodyLargeMedium = _base.copyWith(
     fontSize: 14,
     fontWeight: FontWeight.w300,
     height: 1.56,
   );
 
-  /// bodyLargeRegular Text Style
   static TextStyle bodyLargeRegular = _base.copyWith(
     fontSize: 12,
     fontWeight: FontWeight.w200,
     height: 1.56,
   );
 
-  /// labelLarge Text Style
   static TextStyle labelLarge = _base.copyWith(
     fontSize: 14,
     fontWeight: FontWeight.w300,
@@ -105,7 +92,6 @@ class RestaurantTextStyles {
     letterSpacing: 1.3,
   );
 
-  /// labelMedium Text Style
   static TextStyle labelMedium = _base.copyWith(
     fontSize: 12,
     fontWeight: FontWeight.w200,
@@ -113,7 +99,6 @@ class RestaurantTextStyles {
     letterSpacing: 1.3,
   );
 
-  /// labelSmall Text Style
   static TextStyle labelSmall = _base.copyWith(
     fontSize: 11,
     fontWeight: FontWeight.w100,

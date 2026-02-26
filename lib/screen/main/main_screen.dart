39f7dc17f8b6b163e95c6b1c355005264daa29ec
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-26 18:09:52 +0800

    add bottom navigation bar again in main.dart

--- lib/screen/main/main_screen.dart
+++ lib/screen/main/main_screen.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_restaurant_app/provider/main/index_nav_provider.dart';
+import 'package:flutter_restaurant_app/screen/favorite/favorite_screen.dart';
 import 'package:flutter_restaurant_app/screen/home/home_screen.dart';
+import 'package:flutter_restaurant_app/screen/settings/settings_screen.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: HomeScreen());
+    return Scaffold(
+      body: Consumer<IndexNavProvider>(
+        builder: (context, value, child) {
+          return switch (value.indexBottomNavBar) {
+            0 => const HomeScreen(),
+            1 => const FavoriteScreen(),
+            2 => const SettingsScreen(),
+            _ => const HomeScreen(),
+          };
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: context.watch<IndexNavProvider>().indexBottomNavBar,
+        onTap: (index) {
+          context.read<IndexNavProvider>().setIndextBottomNavBar = index;
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            tooltip: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+            tooltip: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            tooltip: 'Settings',
+          ),
+        ],
+      ),
+    );
   }
 }

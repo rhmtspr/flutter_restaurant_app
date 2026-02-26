6e85504364af12838dbb07e93eaec3655971608f
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-27 00:24:10 +0800

    create favorite screen

--- lib/screen/favorite/favorite_screen.dart
+++ lib/screen/favorite/favorite_screen.dart
@@ -1,10 +1,59 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_restaurant_app/provider/favorite/local_database_provider.dart';
+import 'package:flutter_restaurant_app/screen/home/restaurant_card_widget.dart';
+import 'package:flutter_restaurant_app/static/navigation_route.dart';
+import 'package:provider/provider.dart';
 
-class FavoriteScreen extends StatelessWidget {
+class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
 
   @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
+  void initState() {
+    Future.microtask(() {
+      context.read<LocalDatabaseProvider>().loadAllRestaurant();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorite List')),
+      body: Consumer<LocalDatabaseProvider>(
+        builder: (context, value, child) {
+          final favoriteList = value.restaurantList ?? [];
+          return switch (favoriteList.isNotEmpty) {
+            true => ListView.builder(
+              itemCount: favoriteList.length,
+              itemBuilder: (context, index) {
+                final restaurant = favoriteList[index];
+
+                return RestaurantCard(
+                  restaurant: restaurant,
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      NavigationRoute.detailRoute.name,
+                      arguments: restaurant.id,
+                    );
+                  },
+                );
+              },
+            ),
+            _ => const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text('No Favorite yet')],
+              ),
+            ),
+          };
+        },
+      ),
+    );
   }
 }

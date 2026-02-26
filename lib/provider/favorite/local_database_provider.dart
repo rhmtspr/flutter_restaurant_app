8aa41491af5cf9774fb7c3016c36f01ede5b7e6c
rachmatrisky <rachmatrisky5@gmail.com>
2026-02-26 18:09:11 +0800

    create local_database_provider

---
+++ lib/provider/favorite/local_database_provider.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_restaurant_app/data/local/local_database_service.dart';
+import 'package:flutter_restaurant_app/data/model/restaurant_model.dart';
+
+class LocalDatabaseProvider extends ChangeNotifier {
+  final LocalDatabaseService _service;
+
+  LocalDatabaseProvider(this._service);
+
+  String _message = '';
+  String get message => _message;
+
+  List<Restaurant>? _restaurantList;
+  List<Restaurant>? get restaurantList => _restaurantList;
+
+  Restaurant? _restaurant;
+  Restaurant? get restaurant => _restaurant;
+
+  Future<void> saveRestaurant(Restaurant value) async {
+    try {
+      final result = await _service.insertItem(value);
+
+      final isError = result == 0;
+      if (isError) {
+        _message = 'Failed to save your data';
+      } else {
+        _message = 'Your data is saved';
+      }
+    } catch (e) {
+      _message = 'Failed to save your data';
+    }
+    notifyListeners();
+  }
+
+  Future<void> loadAllRestaurant() async {
+    try {
+      _restaurantList = await _service.getAllItems();
+      _restaurant = null;
+      _message = 'All of your data is loaded';
+    } catch (e) {
+      _message = 'Failed to load your all data';
+      notifyListeners();
+    }
+  }
+
+  Future<void> loadRestaurantById(String id) async {
+    try {
+      _restaurant = await _service.getItemById(id);
+      _message = 'Your data is loaded';
+      notifyListeners();
+    } catch (e) {
+      _message = 'Failed to load your data';
+      notifyListeners();
+    }
+  }
+
+  Future<void> removeRestaurantById(String id) async {
+    try {
+      await _service.removeItem(id);
+      _message = 'Your data is removed';
+      notifyListeners();
+    } catch (e) {
+      _message = 'Failed to remove your data';
+      notifyListeners();
+    }
+  }
+
+  bool checkItemFavorite(String id) {
+    final isSameRestaurant = _restaurant?.id == id;
+    return isSameRestaurant;
+  }
+}
